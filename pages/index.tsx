0213edf9c4d359cf269568910fc39b0284d76394
Anvarjon <anvarjondevelop@gmail.com>
2026-05-03 16:10:24 +0900

    feat: SASS integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
 import withLayoutMain from "@/libs/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
 import { NextPage } from "next";
+import { ST } from "next/dist/shared/lib/utils";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Container>
+      <Stack>
         <Stack flexDirection={"column"}>
-          <Box>Popular properties </Box>
-          <Box>Top Agents</Box>
-          <Box> Top properties</Box>
-          <Box> Top Events</Box>
+          <Stack>
+            <Stack className="container">Popular properties </Stack>
+          </Stack>
+          <Stack>
+            <Stack className="container">Top Agents </Stack>
+          </Stack>
+          <Stack>
+            <Stack className="container">Top properties </Stack>
+          </Stack>
+          <Stack>
+            <Stack className="container">Top Events </Stack>
+          </Stack>
         </Stack>
-      </Container>
+      </Stack>
     </>
   );
 };
